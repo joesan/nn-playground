ac97032414aa837668fc8fd5be7330fae4f3a5ff
joesan <codeintheopen@gmail.com>
2024-05-12 07:37:07 +0200

    'added changes'

--- src/models/is_it_a_bird/train_model.py
+++ src/models/is_it_a_bird/train_model.py
@@ -1,7 +1,15 @@
 from duckduckgo_search import DDGS
+from fastai.data.block import DataBlock, CategoryBlock
+from fastai.data.transforms import get_image_files, RandomSplitter, parent_label
+from fastai.metrics import error_rate
+from fastai.vision.augment import Resize
+from fastai.vision.data import ImageBlock
+from fastai.vision.learner import vision_learner
 from fastai.vision.utils import *
 from fastcore.all import *
 from time import sleep
+
+from torchvision.models import resnet18
 
 searches = 'forest', 'bird'
 path = Path('../data/raw/bird_or_not')
@@ -27,3 +35,27 @@ def fetch_images(searchTerms=searches):
         download_images(dest, urls=search_images(f'{o} shade photo'))
         sleep(10)
         resize_images(path / o, max_size=400, dest=path / o)
+
+
+def unlink_failed_images():
+    failed = verify_images(get_image_files(path))
+    failed.map(Path.unlink)
+    len(failed)
+
+
+def data_block():
+    return DataBlock(
+        blocks=(ImageBlock, CategoryBlock),
+        get_items=get_image_files,
+        splitter=RandomSplitter(valid_pct=0.2, seed=42),
+        get_y=parent_label,
+        item_tfms=[Resize(192, method='squish')]
+    ).dataloaders(path)
+
+
+dls = data_block()
+
+def train_model_from_data():
+    learner = vision_learner(dls, resnet18, metrics=error_rate)
+    learner.fine_tune(3)
+    return learner
