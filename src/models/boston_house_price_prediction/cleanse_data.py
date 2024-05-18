b533cf6f7d30f9b882988d43e6f31111d6f978aa
joesan <codeintheopen@gmail.com>
2024-05-18 07:30:55 +0200

    'added changes'

--- src/models/boston_house_price_prediction/cleanse_data.py
+++ src/models/boston_house_price_prediction/cleanse_data.py
@@ -2,23 +2,36 @@
 
 # delete columns with a single unique value
 def delete_unique_columns(df):
-    print(df.shape)
+    print("************+ delete_unique_columns ************+ ")
+    print("Shape of boston before deleting unique columns", df.shape)
     # get number of unique values for each column
     counts = df.nunique()
-    # record columns to delete
-    to_del = [i for i,v in enumerate(counts) if v == 1]
-    # drop useless columns
-    df.drop(to_del, axis=1, inplace=True)
-    print(df.shape)
-    return df
+    print("Are there any columns with unique values?", counts == 1)
+    if (counts == 1):
+        # record columns to delete
+        to_del = [i for i,v in enumerate(counts) if v == 1]
+        # drop useless columns
+        df.drop(to_del, axis=1, inplace=True)
+        print("Shape of boston after deleting unique columns", df.shape)
+        print("************+ delete_unique_columns ************+ ")
+    else:
+        print("************+ delete_unique_columns ************+ ")
+        return df
 
+
+# delete rows containing duplicate data from the dataset
 def delete_duplicate_rows(df):
-    # delete rows of duplicate data from the dataset
-    from pandas import read_csv
-    # load the dataset
-    df = read_csv(' iris.csv ', header=None)
-    print(df.shape)
+    print("************+ delete_duplicate_rows ************+ ")
     # delete duplicate rows
-    df.drop_duplicates(inplace=True)
-    print(df.shape)
-    return df
+    dups = df.duplicated()
+    # report if there are any duplicates
+    print("Are there any duplicates? ", dups.any())
+    if (dups.any()):
+        print(df[dups])
+        df.drop_duplicates(inplace=True)
+        print(df.shape)
+        print("************+ delete_duplicate_rows ************+ ")
+        return df
+    else:
+        print("************+ delete_duplicate_rows ************+ ")
+        return df
