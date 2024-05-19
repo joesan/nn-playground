7773ab15dcef2ee1e3851f815a97b94bd403e0d5
joesan <codeintheopen@gmail.com>
2024-05-19 10:24:49 +0200

    'added code'

--- README.md
+++ README.md
@@ -54,4 +54,8 @@ Project Organization
 
 --------
 
+# To Install Dependencies
+
+pip install -r requirements.txt 
+
 <p><small>Project based on the <a target="_blank" href="https://drivendata.github.io/cookiecutter-data-science/">cookiecutter data science project template</a>. #cookiecutterdatascience</small></p>
--- src/models/boston_house_price_prediction/cleanse_data.py
+++ src/models/boston_house_price_prediction/cleanse_data.py
@@ -1,27 +1,28 @@
+from colorama import Fore, Back, Style
 
 
 # delete columns with a single unique value
 def delete_unique_columns(df):
-    print("************+ delete_unique_columns ************+ ")
+    print(Fore.RED, "************+ delete_unique_columns ************+ ")
     print("Shape of boston before deleting unique columns", df.shape)
     # get number of unique values for each column
     counts = df.nunique()
-    print("Are there any columns with unique values?", counts == 1)
-    if (counts == 1):
+    print(Back.GREEN, "Are there any columns with unique values?", (counts == 1).any())
+    if (counts == 1).any():
         # record columns to delete
         to_del = [i for i,v in enumerate(counts) if v == 1]
         # drop useless columns
         df.drop(to_del, axis=1, inplace=True)
         print("Shape of boston after deleting unique columns", df.shape)
-        print("************+ delete_unique_columns ************+ ")
+        print(Fore.RED, "************+ delete_unique_columns ************+ ")
     else:
-        print("************+ delete_unique_columns ************+ ")
+        print(Fore.RED, "************+ delete_unique_columns ************+ ")
         return df
 
 
 # delete rows containing duplicate data from the dataset
 def delete_duplicate_rows(df):
-    print("************+ delete_duplicate_rows ************+ ")
+    print(Fore.RED, "************+ delete_duplicate_rows ************+ ")
     # delete duplicate rows
     dups = df.duplicated()
     # report if there are any duplicates
@@ -30,8 +31,8 @@ def delete_duplicate_rows(df):
         print(df[dups])
         df.drop_duplicates(inplace=True)
         print(df.shape)
-        print("************+ delete_duplicate_rows ************+ ")
+        print(Fore.RED, "************+ delete_duplicate_rows ************+ ")
         return df
     else:
-        print("************+ delete_duplicate_rows ************+ ")
+        print(Fore.RED, "************+ delete_duplicate_rows ************+ ")
         return df
